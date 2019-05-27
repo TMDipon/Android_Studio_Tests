96d7b00fb4cd6b1e49e44999016ccd5cb88acdd8
TMDipon <tmdcse15@gmail.com>
2019-05-27 11:30:25 +0600

    Currency Converter completed

---
+++ CurrencyConverter/app/src/main/java/com/example/dell/currencyconverter/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.dell.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert(View v)
+    {
+        EditText t = (EditText)findViewById(R.id.dollar);
+        String t1 = t.getText().toString();
+        Double dol = Double.parseDouble(t1);
+        Double pound = dol * 0.79;
+        TextView x = (TextView) findViewById(R.id.pound);
+        x.setText("Amount in Pound: "+pound.toString());
+        Toast.makeText(this, "Dollar to Pound converted", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
